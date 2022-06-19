e982b5c4435ef3f523c867744f414ae383cfb733
Adrien <adrien.biz@gmail.com>
2022-06-19 15:03:32 +0400

    Create random selection function

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-// Randomly assign a shape (rock, paper or scissors) and store it in a variable
-const shapes = ["rock", "paper", "scissors"];
-const randomShape = shapes[Math.floor(Math.random() * shapes.length)];
-console.log(randomShape)
+function computerPlay() {
+    const shapes = ["rock", "paper", "scissors"];
+    return shapes[Math.floor(Math.random() * shapes.length)];
+}
 
-// Prompt user for a shape
-const userSelection = prompt("Please chose a shape between rock, paper, and scissors")
-console.log(userSelection)
+// const playerSelection = prompt("Please chose a shape between rock, paper, and scissors")
+// console.log(playerSelection)
 
-// if (userSelection === "") {
+// if (playerSelection === "") {
 //     alert("Empty value");
-// } else if (!isNaN(userSelection))
+// } else if (!isNaN(playerSelection))
 //     alert("Value is not a string");
 
-playRound(userSelection, randomShape)
+// let computer = computerPlay();
+// console.log(computer);
+// console.log("rock");
 
-// Compare the 2 shapes and display results: draw, win or loss
-function playRound(userSelection, computerSelection) {
-    if (userSelection === "rock" && computerSelection === "scissors"){
-        alert("Rock crushes scissors, player wins!")
-    } else if (userSelection === "rock" && computerSelection === "paper"){
-        alert("Paper covers rock, player loses!")
-    } else if (userSelection === "scissors" && computerSelection === "paper") {
-        alert("Scissors cuts paper, player wins!")
-    } else if (userSelection === "scissors" && computerSelection === "rock") {
-        alert("Rock crushes scissors, player loses!")
-    } else if (userSelection === "paper" && computerSelection === "rock") {
-        alert("Paper covers rock, player wins!")
-    } else if (userSelection === "paper" && computerSelection === "scissors") {
-        alert("Scissors cuts paper, player loses!")
+// console.log(playRound("rock", computer));
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "scissors"){
+        return "rock crushes scissors, player wins!";
+    } else if (playerSelection === "rock" && computerSelection === "paper"){
+        return "paper covers rock, player loses!";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "scissors cuts paper, player wins!";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "rock crushes scissors, player loses!";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "paper covers rock, player wins!";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "scissors cuts paper, player loses!";
     } else {
-        alert("It's a tie!")
+        return "It's a tie!";
     }
 }
