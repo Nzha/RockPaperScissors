3930bfa53132564c9cd2269c61a40fe2df0be44f
Adrien <adrien.biz@gmail.com>
2022-06-20 13:51:32 +0400

    Sanitize user input

--- script.js
+++ script.js
@@ -3,20 +3,6 @@ function computerPlay() {
     return shapes[Math.floor(Math.random() * shapes.length)];
 }
 
-// const playerSelection = prompt("Please chose a shape between rock, paper, and scissors")
-// console.log(playerSelection)
-
-// if (playerSelection === "") {
-//     alert("Empty value");
-// } else if (!isNaN(playerSelection))
-//     alert("Value is not a string");
-
-// let computer = computerPlay();
-// console.log(computer);
-// console.log("rock");
-
-// console.log(playRound("rock", computer));
-
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "scissors"){
         return "You win! Rock crushes scissors";
@@ -30,8 +16,21 @@ function playRound(playerSelection, computerSelection) {
         return "You win! Paper covers rock!";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         return "You lose! Scissors cuts paper";
-    } else {
+    } else if (playerSelection === computerSelection) {
         return "It's a tie!";
+    } else {
+        return "Invalid shape";
+    }
+}
+
+function userInput() {
+    const input = prompt("Please chose a shape between rock, paper, and scissors")
+    const playerSelection = input.toLowerCase();
+    // console.log(playerSelection);
+    if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
+        return playerSelection;
+    } else {
+        alert("Please insert a valid shape");
     }
 }
 
@@ -40,18 +39,15 @@ let computerScore = 0;
 
 function game(n) {
     for (let i = 0; i < n; i++) {
-        let round = playRound(playerSelection, computerPlay());
+        let round = playRound(userInput(), computerPlay());
         console.log(round);
         if (round.includes("win")) {
             playerScore += 1;
         } else if (round.includes("lose")) {
             computerScore += 1;
         }
+        console.log(`Player score: ${playerScore}\nComputer score: ${computerScore}`)
     }
 }
 
-playerSelection = "rock";
 game(5);
-
-console.log(playerScore);
-console.log(computerScore);
