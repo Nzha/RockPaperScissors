8ab38df8c5a3c12b55452abdbeb15e24fe1fb3e0
Adrien <adrien.biz@gmail.com>
2022-06-28 21:47:40 +0400

    Create base game functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,3 @@
-// function playRound(playerSelection, computerSelection) {
-//     if (playerSelection === "rock" && computerSelection === "scissors"){
-//         return "You win! Rock crushes scissors";
-//     } else if (playerSelection === "rock" && computerSelection === "paper"){
-//         return "You lose! Paper covers rock";
-//     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-//         return "You win! Scissors cuts paper";
-//     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-//         return "You lose! Rock crushes scissors";
-//     } else if (playerSelection === "paper" && computerSelection === "rock") {
-//         return "You win! Paper covers rock!";
-//     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-//         return "You lose! Scissors cuts paper";
-//     } else {
-//         return "It's a tie!";
-//     }
-// }
-
-// let playerScore = 0;
-// let computerScore = 0;
-
 // function game(n) {
 //     for (let i = 0; i < n; i++) {
 //         let round = playRound(userInput(), computerPlay());
@@ -44,11 +23,17 @@
 
 
 const buttons = document.querySelectorAll('button');
-const score = document.querySelector('.score .text');
+const result = document.querySelector('.result .text');
+const playerScoreDisplay = document.querySelector('#player-score');
+const computerScoreDisplay = document.querySelector('#computer-score');
+let playerScore = 0;
+let computerScore = 0;
 
 buttons.forEach(button => button.addEventListener('click', (e) => {
-    playerPlay(e);
-    score.textContent = 'test';
+    // result.textContent = playRound(playerPlay(e), computerPlay());
+    result.textContent = playGame(e);
+    playerScoreDisplay.textContent = `Player score: ${playerScore}`;
+    computerScoreDisplay.textContent = `Computer score: ${computerScore}`;
 }));
 
 function computerPlay() {
@@ -61,6 +46,33 @@ function playerPlay(e) {
      * Store user's shape wherever button is clicked
      * (on the emoji, the text, or outside those elements but inside the button div) 
      */
-     let playerShape = e.target.closest('.shape').id;
-     console.log(playerShape); 
+     return playerShape = e.target.closest('.shape').id;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "scissors"){
+        return "You win! Rock crushes scissors";
+    } else if (playerSelection === "rock" && computerSelection === "paper"){
+        return "You lose! Paper covers rock";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors cuts paper";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock crushes scissors";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper covers rock!";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors cuts paper";
+    } else {
+        return "It's a tie!";
+    }
+}
+
+function playGame(e) {
+    let round = playRound(playerPlay(e), computerPlay());
+    if (round.includes("win")) {
+        playerScore += 1;
+    } else if (round.includes("lose")) {
+        computerScore += 1;
+    }
+    return round;
 }
